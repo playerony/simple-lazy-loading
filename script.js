627154536011d1870df41a9f48b21f7d90c5b8f6
playerony <pawel.wojtasinski.1995@gmail.com>
2021-09-13 20:42:29 +0200

    process lazy loading

--- script.js
+++ script.js
@@ -20,8 +20,48 @@ function scrollToTop() {
   }
 }
 
+function switchSrcAttributesForElement(element) {
+  if (!element) {
+    return null
+  }
+
+  const dataSrc = element.getAttribute('data-src')
+  if (dataSrc) {
+    element.setAttribute('src', dataSrc);
+    element.removeAttribute("data-src");
+  }
+}
+
+function processLazyElements() {
+  const lazyElements = document.querySelectorAll('[data-src]')
+
+  if ("IntersectionObserver" in window) {
+    const lazyObserver = new IntersectionObserver((entries) => {
+      entries.forEach(_entry => {
+        if(_entry.isIntersecting) {
+          const element = _entry.target;
+    
+          switchSrcAttributesForElement(element);
+          lazyObserver.unobserve(element);
+        }
+      })
+    })
+
+    lazyElements.forEach(_element => {
+      if (_element.tagName === 'SCRIPT') {
+        switchSrcAttributesForElement(_element)
+      } else {
+        lazyObserver.observe(_element)
+      }
+    })
+  } else {
+    lazyElements.forEach(switchSrcAttributesForElement)
+  }
+}
+
 function onReady() {
   scrollToTop()
+  processLazyElements()
 }
 
 documentReady(onReady)
